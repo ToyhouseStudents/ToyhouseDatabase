b2d02496c8cc0256c63bcbc50c1daaa2aba10dfc
Pan Tao <pantaovay@gmail.com>
2012-10-23 14:15:45 +0800

    Init code

---
+++ code/phprojekt/tests/UnitTests/Default/Controllers/TagControllerTest.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Unit test
+ *
+ * This software is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 3 as published by the Free Software Foundation
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * @category   PHProjekt
+ * @package    UnitTests
+ * @subpackage Default
+ * @copyright  Copyright (c) 2010 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.0
+ * @version    Release: 6.1.0
+ * @author     Gustavo Solt <solt@mayflower.de>
+ */
+
+
+/**
+ * Tests for Tag Controller
+ *
+ * @category   PHProjekt
+ * @package    UnitTests
+ * @subpackage Default
+ * @copyright  Copyright (c) 2010 Mayflower GmbH (http://www.mayflower.de)
+ * @license    LGPL v3 (See LICENSE file)
+ * @link       http://www.phprojekt.com
+ * @since      File available since Release 6.0
+ * @version    Release: 6.1.0
+ * @author     Gustavo Solt <solt@mayflower.de>
+ * @group      default
+ * @group      controller
+ * @group      default-controller
+ */
+class Phprojekt_TagController_Test extends FrontInit
+{
+    protected function getDataSet() {
+        return $this->createFlatXMLDataSet(dirname(__FILE__) . '/../data.xml');
+    }
+
+    /**
+     * Test of json get tags
+     */
+    public function testJsonGetTagsAction()
+    {
+        $this->setRequestUrl('Default/Tag/jsonGetTags/');
+        $this->request->setParam('nodeId', 1);
+        $this->request->setParam('limit', 2);
+        $response = FrontInit::phprJsonToArray($this->getResponse());
+        $expected = array(
+            'metadata' => array(
+                array(
+                    'key'   => 'string',
+                    'label' => 'Tags',
+                ),
+                array(
+                    'key'   => 'count',
+                    'label' => 'Count',
+                ),
+            ),
+            'data' => array(
+                array(
+                    'string' => 'this',
+                    'count'  => '3',
+                ),
+            ),
+            'numRows' => 1,
+        );
+        $this->assertEquals($expected, $response);
+    }
+
+    /**
+     * Test of GetModulesByTag
+     */
+    public function testJsonSaveTagsAction()
+    {
+        $this->setRequestUrl('Default/Tag/jsonSaveTags/');
+        $this->request->setParam('moduleName', 'Project');
+        $this->request->setParam('string', 'test');
+        $this->request->setParam('id', 1);
+        $this->request->setParam('projectId', 1);
+        $this->getResponse();
+    }
+
+    /**
+     * Test of GetModulesByTag
+     */
+    public function testJsonGetModulesByTagAction()
+    {
+        $this->setRequestUrl('Default/Tag/jsonGetModulesByTag/');
+        $this->request->setParam('nodeId', 1);
+        $this->request->setParam('tag', 'this');
+        $this->request->setParam('limit', 2);
+        $response = $this->getResponse();
+        $expected = '{"id":1,"moduleId":1,"moduleName":"Project","moduleLabel":"Project","firstDisplay":"test",'
+            . '"secondDisplay":null,"projectId":1}';
+        $this->assertContains($expected, $response);
+    }
+}
---
+++ code/toyhouse/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Toyhouse</title>
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+<style>
+body {
+	padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+}
+</style>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css" />
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+<body>
+	<div>
+		<img src="./img/logo_big.png" alt="toyhouse" />
+		<h1>Welcome to Toyhouse !</h1>
+	</div>
+	<div>
+		<h2>Softwares we have provided:</h2>
+		<ul>	
+			<li>Mediawiki: <a href="http://toyhouse.ie.tsinghua.edu.cn/mediawiki/" target="_blank">http://toyhouse.ie.tsinghua.edu.cn/mediawiki/</a></li>
+			<li>Elgg: <a href="http://toyhouse.ie.tsinghua.edu.cn/elgg/" target="_blank">http://toyhouse.ie.tsinghua.edu.cn/elgg/</a></li>
+			<li>PHProjekt: <a href="http://toyhouse.ie.tsinghua.edu.cn/phprojekt/" target="_blank">http://toyhouse.ie.tsinghua.edu.cn/phprojekt/</a></li>
+			<li>Question2Answer: <a href="http://toyhouse.ie.tsinghua.edu.cn/q2a/" target="_blank">http://toyhouse.ie.tsinghua.edu.cn/q2a/</a></li>
+		</ul>
+		<h2>Previous Videos & Some other things:</h2>
+		<p>We also prepare some interesting videos for you ! They are <a href="ftp://166.111.59.15" target="_blank">here>>></a></p>
+		<h2>Tools:</h2>
+		<ul>
+			<li>Google url shortener: <a href="http://goo.gl/" target="_blank">http://goo.gl/</a></li>
+			<li>OSChina.NET tools:<a href="http://www.ostools.net/" target="_blank">http://www.ostools.net/</a></li>
+			<li>Markdown:<a href="http://wowubuntu.com/markdown/" target="_blank">A brief introduction to Markdown syntax</a></li>
+			<li>W3school:<a href="http://www.w3school.com.cn/" target="_blank">Learning Web online</a></li>
+		</ul>
+		<h2>Learning Materials We Suggest:</h2>
+		<p><a href="./learning_materials.php">Learning Materials</a></p>
+		<h2>Now We are working on ... </h2>
+		<p><a href="http://code.google.com/p/course-builder/" target="_blank">Google course bulider</a> and <a href="https://github.com/" target="_blank">GitHub</a>. They are on the way to you.</p>
+	</div>
+	<div> 
+		<p>Power by <i>We Are SA !</i> Any question, please contact <a href="mailto:pantaovay@gmail.com"><img src="./img/mail.png" alt="pantaovay@gmail.com" /></a></p>
+	</div>
+<body>
+</html>
